af4d49da307f9b62688ede7e788ab7e294d24281
Konstantin K <liroy3000@gmail.com>
2023-09-03 19:37:23 +0300

    add server_name to settings.py

--- modules/alertertg.py
+++ modules/alertertg.py
@@ -3,9 +3,9 @@
 def alerter(tg, message):
     import requests
 
-    format_message = ""
+    format_message = "Server name: " + tg["server_name"]
     for i in message:
-        format_message = format_message + i + ": " + message[i] + "\n"
+        format_message = format_message + i + ": " + str(message[i]) + "\n"
 
     url = 'https://api.telegram.org/bot' + tg["token"] + '/sendMessage'
     params = {'chat_id': tg["chat_id"], 'text': format_message}
